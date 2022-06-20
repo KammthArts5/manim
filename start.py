0928add0339d96fcb633f847a7b411f7ce0dff38
KammthArts5 <jacouton.valentin@gmail.com>
2022-06-20 14:14:03 +0200

    Petits tests Manim

--- start.py
+++ start.py
@@ -1,9 +1,14 @@
 from manimlib import *
 
-class CircleToSquare(Scene):
+class SquareToCircle(Scene):
     def construct(self):
         circle = Circle()
         circle.set_fill(BLUE, opacity=0.5)
-        circle.set_stroke(BLUE_E, width=4)
+        circle.set_stroke(RED, width=4)
+        square = Square()
+        square.set_stroke(WHITE, width=4)
 
-        self.add(Circle)
+        self.play(ShowCreation(square))
+        self.wait()
+        self.play(ReplacementTransform(square, circle))
+        self.wait()
